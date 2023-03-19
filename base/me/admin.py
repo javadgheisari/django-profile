3cc04829fefe93f9ec848890864419dea6197254
javad <javad.gheisarii@gmail.com>
2023-03-19 09:53:05 +0330

    update template for mobiles & django admin

--- base/me/admin.py
+++ base/me/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Blog
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'created', 'updated', 'url')
+    list_display = ('title', 'description', 'created', 'updated')
+    search_fields = ("title__startswith", )
+    list_filter = ("created", "updated")
+    
 admin.site.register(Blog, BlogAdmin)
