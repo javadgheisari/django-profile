3718247eda7771fcd0bc7a9e7d9e20b0530f1dbb
javad <javad.gheisarii@gmail.com>
2024-06-22 14:52:32 +0330

    added keywords in list_filter in BlogAdmin

--- base/me/admin.py
+++ base/me/admin.py
@@ -4,7 +4,7 @@ from .models import Blog, Keyword
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'short_description', 'created', 'updated')
     search_fields = ("title__startswith", )
-    list_filter = ("created", "updated")
+    list_filter = ("created", "updated", "keywords")
     
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(Keyword)
